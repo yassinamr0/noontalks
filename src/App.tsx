00156e3e0a0ff9001498483538bab4696f9468a3
yassinamr0 <yassincemal777@gmail.com>
2025-01-14 23:10:32 +0300

    Fix: Show Index page at root path instead of Register

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Index from '@/pages/Index';
 import Register from '@/pages/Register';
 import Login from '@/pages/Login';
 import Admin from '@/pages/Admin';
@@ -22,7 +23,7 @@ function App() {
     <Router>
       <Toaster position="top-center" richColors />
       <Routes>
-        <Route path="/" element={<Register />} />
+        <Route path="/" element={<Index />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/admin/login" element={<AdminLogin />} />
